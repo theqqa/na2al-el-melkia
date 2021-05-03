aab0de8a475f7693831ad6aed0a14f3b9e7b7d11
mayyoussri93 <mayyoussri93@gmail.com>
2021-05-03 12:29:21 +0200

    live

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -217,6 +217,7 @@ if ($transaction->save()) {
           elseif($request->type==3){
               $total  +=  $representative_data->renewal_price+$representative_data->transfer_price;
           }
+          if(!empty(RepresentativeHistory::whereRepId($request->representative_id)->where('transaction_id',$transaction->id)->first())){
           $representative_data->deserved_amount -= RepresentativeHistory::whereRepId($request->representative_id)->where('transaction_id',$transaction->id)->first()->deserved_amount_request;
 
           $treasury_balance_history= RepresentativeHistory::whereRepId($request->representative_id)->where('transaction_id',$transaction->id)->first();
@@ -224,6 +225,16 @@ if ($transaction->save()) {
           $treasury_balance_history->deserved_amount_after=$representative_data->deserved_amount + $total ;
           $treasury_balance_history->deserved_amount_request=$total;
           $treasury_balance_history->save();
+          }else{
+          
+            $treasury_balance_history= new  RepresentativeHistory();
+        $treasury_balance_history->rep_id= $transaction->representative_id;
+        $treasury_balance_history->transaction_id= $transaction->id;
+        $treasury_balance_history->deserved_amount_before= $representative_data->deserved_amount;
+        $treasury_balance_history->deserved_amount_after=$representative_data->deserved_amount + $total ;
+        $treasury_balance_history->deserved_amount_request=$total;
+        $treasury_balance_history->save();
+          }
 
           $representative_data->deserved_amount +=$total;
           $representative_data->save();
